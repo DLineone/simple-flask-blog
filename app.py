1d6cddd2b33c26c7fcdd28c728e17b045fc5eb82
Dmitriy Lefterov <62502633+DLineone@users.noreply.github.com>
2025-12-03 02:19:28 +0300

    bd 1

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+db = SQLAlchemy(app)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(300), nullable=False)
+    intro = db.Column(db.String(300), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Post %r>' % self.id
 
 
 @app.route('/')
@@ -14,9 +28,9 @@ def about():
     return render_template("about.html")
 
 
-@app.route('/user/<string:name>/<int:id>')
-def user(name, id):
-    return f'user {name} {str(id)}'
+@app.route('/create-post')
+def create_post():
+    return render_template("create_post.html")
 
 
 if __name__ == "__main__":
